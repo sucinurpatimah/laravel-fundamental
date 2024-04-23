ce95ceb1a86f3ae9b0ad3cc1f4b70bd2cb963fec
Suci Nurpatimah <sucinrptmh24@gmail.com>
2024-04-23 14:30:56 +0700

    Menambahkan route di web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list-menu/get-all-data', function () {
+    $menus = [
+        [
+            "menu" => "Soto Mie",
+            "harga" => 15000
+        ],
+
+        [
+            "menu" => "Bakso Malang",
+            "harga" => 12000
+        ],
+
+        [
+            "menu" => "Ketoprak",
+            "harga" => 10000
+        ]
+    ];
+
+    $response = [
+        "info" => "Data Berhasil Didapatkan",
+        "data" => $menus
+    ];
+
+    return response()->json($response, 200, [], JSON_PRETTY_PRINT);
+});
+
+Route::get('/list-menu', function () {
+    $menus = [
+        "Soto Mie",
+        "Bakso Malang",
+        "Ketoprak"
+    ];
+
+    $menuList = implode("<br>- ", $menus);
+
+    return "Ini adalah halaman yang menambilkan semua menu yang ada di Cafe Amandemy:<br>- " . $menuList;
+});
+
+Route::get('/list-menu/bakso/8000', function () {
+    return "Anda memilih makanan bakso dengan harga 8000";
+});
